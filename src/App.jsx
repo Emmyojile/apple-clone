a5fdc8c62c0e3f6e18381d8d3755be6d9ec1577b
Emmyojile <emmaojile99@gmail.com>
2024-04-12 03:38:20 +0100

    models section

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Hero } from "./Components/Hero";
 import { Highlights } from "./Components/Highlights";
 import { Navbar } from "./Components/Navbar";
 import { MyRef } from "./Components/MyRef";
+import Model from "./Components/Model";
 
 const App = () => {
   return (
-    <main className="bg-gray-700">
+    <main>
       <Navbar />
       <Hero />
       <Highlights />
+      <Model/>
       {/* <MyRef/> */}
     </main>
   );
