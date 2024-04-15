c166b19627f03ec4f52fb179b080a937b7f5ac3d
Emmyojile <emmaojile99@gmail.com>
2024-04-15 04:41:22 +0100

    models section

--- src/Components/ModelView.jsx
+++ src/Components/ModelView.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { Html, PerspectiveCamera, View } from '@react-three/drei'
+import React, { Suspense } from 'react'
+import Lights from './Lights'
+import Iphone from './Iphone'
 
-const ModelView = () => {
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <div>ModelView</div>
+    <View
+    index={index}
+    id={gsapType}
+    className={`w-full h-full border-2 border-red-500 absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+
+      <ambientLight intensity={0.3}/>
+
+      <PerspectiveCamera makeDefault position={[0,0,4]}/>
+
+      <Lights/>
+
+      <Suspense fallback={<Html><div>Loading...</div></Html>}>
+        <Iphone/>
+
+      </Suspense>
+    </View>
   )
 }
 
